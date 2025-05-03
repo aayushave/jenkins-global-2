7f0b8fd2c8b644748a0efa2424f625457172f06e
aayushave <31796139+aayushave@users.noreply.github.com>
2025-05-03 21:04:53 +0530

    Update docker_build_windows.groovy

--- vars/docker_build_windows.groovy
+++ vars/docker_build_windows.groovy
@@ -1,1 +1,12 @@
-bat "docker build -t ${imageName}:${imageTag} ."
+def call(Map config = [:]) {
+    def imageName = config.imageName ?: error("Image name is required")
+    def imageTag = config.imageTag ?: 'latest'
+    def dockerfile = config.dockerfile ?: 'Dockerfile'
+    def context = config.context ?: '.'
+
+    bat "echo Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
+
+    bat """
+        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+    """
+}
